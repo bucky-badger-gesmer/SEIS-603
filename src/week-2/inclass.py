7125fc9223a75f8a39512a9fc9169eec53441a7c
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2025-09-26 08:19:52 -0500

    feat: enhance inclass week 2

--- src/week-2/inclass.py
+++ src/week-2/inclass.py
@@ -10,7 +10,7 @@ def count_vowels(s):
         return count
     except TypeError:
         print("Invalid value passed to count_vowels")
-        return None
+        return 0
 
 
 def reverse_words(s):
@@ -23,31 +23,35 @@ def reverse_words(s):
         for i in range(len(words) - 1, -1, -1):
             words_reverse = words_reverse + words[i] + " "
 
-        return words_reverse
+        return words_reverse.strip()
     except AttributeError:
         print("Invalid value passed to reverse_words")
         return None
 
 
-# def is_palindrom(s):
-#     print("original string: ", s)
+def is_palindrome(s):
+    try:
+        for forward_index in range(len(s)):
+            backward_index = len(s) - forward_index - 1
 
-#     for i in range(len(s)):
-#         for j in range(len(s) - 1, -1, -1):
-#             print("i", s[i])
-#             print("j", s[j])
-#             if i == j:
-#                 break
-#             if s[i] != s[j]:
-#                 return False
-#     return True
+            if forward_index == backward_index:
+                break
+
+            if s[forward_index].lower() != s[backward_index].lower():
+                return False
+
+        return True
+    except TypeError:
+        print("Invalid value passed to is_palindrome")
+        return False
 
 
 if __name__ == "__main__":
+    pass
     # vowel_count = count_vowels(12.3333)
     # print("vowel_count: ", vowel_count)
 
     # reverse_words(12)
 
-    foo = is_palindrom("racecar")
-    print("foo: ", foo)
+    # foo = is_palindrom(1)
+    # print("foo: ", foo)
