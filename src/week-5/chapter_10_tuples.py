346255a1c59fa2fbbf30ddc47a766c644daaf770
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2025-10-04 09:59:53 -0500

    feat: add code for chapter 10 exercises

--- src/week-5/chapter_10_tuples.py
+++ src/week-5/chapter_10_tuples.py
@@ -1,16 +1,47 @@
-txt = "but soft what light in yonder window breaks"
-words = txt.split()
+file_name = input("Enter file name: ")
 
-t = list()
+try:
+    fh = open(file_name)
+except FileNotFoundError:
+    print("File not found. Enter a valid file name.")
+    exit()
 
-for word in words:
-    t.append((len(word), word))
-print("t", t)
+hours = {}
 
-t.sort(reverse=True)
-res = list()
+for line in fh:
+    if "From " not in line:
+        continue
 
-for length, word in t:
-    res.append(word)
+    line_split = line.split(":")
+    time_split = line_split[0].split()
+    hour = time_split[len(time_split) - 1]
 
-print(res)
+    hours[hour] = hours.get(hour, 0) + 1
+
+
+# print(hours)
+
+hours_list = list(hours.items())
+hours_list.sort()
+
+
+for key, value in hours_list:
+    print(key, value)
+
+
+# txt = "but soft what light in yonder window breaks"
+# words = txt.split()
+
+# t = list()
+
+# for word in words:
+#     t.append((len(word), word))
+# print("t", t)
+
+# t.sort(reverse=True)
+# res = list()
+
+# for length, word in t:
+#     res.append(word)
+
+# print(res)
