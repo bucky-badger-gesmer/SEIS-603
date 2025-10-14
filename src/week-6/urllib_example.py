2bae635e3ad3b557ae968ecf74fbd5839dbf9e24
Aaron Gesmer <agesmer@augeomarketing.com>
2025-10-14 15:32:33 -0500

    feat: add urllib example

---
+++ src/week-6/urllib_example.py
@@ -0,0 +1,20 @@
+# import urllib.request
+
+# fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
+
+# for line in fhand:
+#     print(line.decode().strip())
+
+
+import urllib.error
+import urllib.parse
+import urllib.request
+
+fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
